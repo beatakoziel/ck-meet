44e021585783a2ab1bff44680c46801a812e5403
Beata <beatainez.k@gmail.com>
2020-10-14 22:23:51 +0200

    initial commit

---
+++ src/main/java/com/meet/ck/CkApplication.java
@@ -0,0 +1,13 @@
+package com.meet.ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkApplication.class, args);
+    }
+
+}
