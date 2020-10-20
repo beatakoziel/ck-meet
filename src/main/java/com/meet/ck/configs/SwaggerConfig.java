cbd214e75dc656d84019610df562a05fdc0e5157
Beata <beatainez.k@gmail.com>
2020-10-20 11:15:21 +0200

    updated swagger

--- src/main/java/com/meet/ck/configs/SwaggerConfig.java
+++ src/main/java/com/meet/ck/configs/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.meet.ck.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,14 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
-    public Docket get() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket api() {
+        return (new Docket(DocumentationType.SWAGGER_2))
+                .host("localhost:8080")
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
-
 }
